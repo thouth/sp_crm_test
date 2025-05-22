b9cc171848e1668452d4b0f2ed48abab08ee7569
thouth <78444186+thouth@users.noreply.github.com>
2025-05-22 07:02:20 +0200

    Create parseExcel.js

---
+++ utils/parseExcel.js
@@ -0,0 +1,14 @@
+import * as XLSX from "xlsx";
+
+export function parseExcel(file, cb) {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: "array" });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const json = XLSX.utils.sheet_to_json(worksheet);
+    cb(json);
+  };
+  reader.readAsArrayBuffer(file);
+}
